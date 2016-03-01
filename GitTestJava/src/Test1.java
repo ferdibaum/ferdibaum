bb60d05872a8eb3c39f1279dc6b8cbfed32cc302
ferdibaum <ferdi99@live.de>
2016-03-01 11:30:53 +0100

    hallo

--- GitTestJava/src/Test1.java
+++ GitTestJava/src/Test1.java
@@ -4,6 +4,6 @@ public class Test1 {
 	public static void main(String[] args){
 		System.out.println("Hello");
 		System.out.println("Hello1");
-		//test1 sjkdföasldj
+		//test1 hallo hallo hallo ina ina ina 
 	}
 }
